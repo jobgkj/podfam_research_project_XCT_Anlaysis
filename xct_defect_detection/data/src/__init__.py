79afc878b86f2ed575297054e739f12643809f85
Job George K J <jobgeorgekj@gmail.com>
2026-05-12 16:08:07 +0900

    Create __init__.py

---
+++ xct_defect_detection/data/src/__init__.py
@@ -0,0 +1,7 @@
+"""
+XCT Defect Segmentation Package
+
+This package provides utilities for preprocessing, threshold-based
+segmentation, metric computation, and visualization of defects in
+X-ray Computed Tomography (XCT) data of additively manufactured metal parts.
+"""
